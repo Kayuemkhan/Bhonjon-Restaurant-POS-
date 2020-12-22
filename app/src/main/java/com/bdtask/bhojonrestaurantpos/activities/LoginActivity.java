8e8c3e045fb1767eab02bcf1d6c2daf55ee22eef
Kayuemkhan <abdulkayuem007@gmail.com>
2020-12-22 17:41:29 +0600

    1.1

---
+++ app/src/main/java/com/bdtask/bhojonrestaurantpos/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.bdtask.bhojonrestaurantpos.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bdtask.bhojonrestaurantpos.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/bdtask/bhojonrestaurantpos/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bdtask.bhojonrestaurantpos.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bdtask.bhojonrestaurantpos.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
