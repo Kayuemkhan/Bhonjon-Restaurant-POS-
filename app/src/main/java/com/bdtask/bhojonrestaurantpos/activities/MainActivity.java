47324076311451c863dec6dc112680fd9a9dabcf
Kayuemkhan <abdulkayuem007@gmail.com>
2020-12-26 11:00:02 +0600

    "1.1"

--- app/src/main/java/com/bdtask/bhojonrestaurantpos/activities/MainActivity.java
+++ app/src/main/java/com/bdtask/bhojonrestaurantpos/activities/MainActivity.java
@@ -3,23 +3,48 @@ package com.bdtask.bhojonrestaurantpos.activities;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
+import com.bdtask.bhojonrestaurantpos.activities.retrofit.WaiterService;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bdtask.bhojonrestaurantpos.R;
+import com.bdtask.bhojonrestaurantpos.modelClass.Category.CategoryResponse;
+import com.bdtask.bhojonrestaurantpos.retrofit.AppConfig;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView subcategoryName;
+    private RecyclerView subcategoryName;
+    private WaiterService waiterService;
+    private String id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         subcategoryName = findViewById(R.id.subcategoryName);
-        subcategoryName.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayout.HORIZONTAL,false));
+        subcategoryName.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayout.HORIZONTAL, false));
         getSubCategoryName();
+        waiterService = AppConfig.getRetrofit().create(WaiterService.class);
+        id = SharedPref.read("ID", "");
     }
-    public void getSubCategoryName(){
+
+    public void getSubCategoryName() {
+        waiterService.getAllCategories(id).enqueue(new Callback<CategoryResponse>() {
+            @Override
+            public void onResponse(Call<CategoryResponse> call, Response<CategoryResponse> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<CategoryResponse> call, Throwable t) {
+
+            }
+        });
     }
 }
